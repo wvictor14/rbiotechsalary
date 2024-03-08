20fcfd313cdfe1e204e1b89c9c9bef5beee61a59
Victor Yuan <victor.2wy@gmail.com>
2024-03-07 22:46:05 -0800

    add makecard plotly

--- R/launch_app.R
+++ R/launch_app.R
@@ -71,4 +71,18 @@ rb_server <- function(input, output, session) {
       div(style="max-height: 500px; overflow: auto", items_list)
     )
   })
+
+}
+
+#' @export
+makeCard <- function(title, content, size = 12, style = "") {
+  div(
+    class = glue("card ms-depth-8 ms-sm{size} ms-xl{size}"),
+    style = style,
+    Stack(
+      tokens = list(childrenGap = 5),
+      Text(variant = "large", title, block = TRUE),
+      content
+    )
+  )
 }
