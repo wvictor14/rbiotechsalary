dabff9f7e65fac7696cad7776703db859d2383ae
Victor Yuan <victor.2wy@gmail.com>
2024-03-07 18:07:29 -0800

    add table

--- R/launch_app.R
+++ R/launch_app.R
@@ -1,4 +1,12 @@
-launch_app <- function(...) {
+#' @export
+#' @import shiny shiny.fluent
+launch_app <- function(..., salaries = NULL) {
+
+  if (is.null(data)) {
+    data(salaries)
+    salaries |>  glimpse()
+    message('salaries loaded')
+  }
 
   version <- paste0('v', as.character(utils::packageVersion('r_biotech_salary')))
   #shiny::addResourcePath("www", "www")
@@ -16,6 +24,8 @@ rb_ui <- function() {
   shiny.fluent::fluentPage(
     shiny.fluent::Text(variant = "xxLarge", "r/biotech Salary Tracker is live!")
   )
+
+  uiOutput("analysis")
 }
 
 #' app server
@@ -25,4 +35,30 @@ rb_ui <- function() {
 #' @export
 rb_server <- function(input, output, session) {
 
+  .salaries <- reactive({
+    salaries |>
+      select(timestamp:day, salary_base:bonus, everything())
+  })
+
+  output$analysis <- renderUI({
+    #browser()
+    items_list <- if(nrow(.salaries()) > 0){
+      shiny.fluent::DetailsList(
+        items = .salaries(),
+        columns = tibble(
+          fieldName = colnames(.salaries()),
+          name =  colnames(.salaries()),
+          key = fieldName
+          )
+      )
+    } else {
+      p("No matching salary data.")
+    }
+
+    shiny.fluent::Stack(
+      tokens = list(childrenGap = 5),
+      shiny.fluent::Text(variant = "large", "Salary details", block = TRUE),
+      div(style="max-height: 500px; overflow: auto", items_list)
+    )
+  })
 }
