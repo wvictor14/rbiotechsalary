3f68507261a4ce157a1a38385e996c0a6e4999e1
Victor Yuan <victor.2wy@gmail.com>
2024-04-02 22:59:07 -0700

    modularize filters

--- R/dev_shinydashboard.R
+++ R/dev_shinydashboard.R
@@ -2,6 +2,8 @@
 #' @export
 #' @examples \dontrun{
 #'   launch_app(ui = rb_ui2, server = function(input, output) {})
+#'   launch_app(ui = rb_ui2, server = rb_server_2)
+#'   
 #' }
 rb_ui2 <- function() {
   link_github <- tags$a(
@@ -15,7 +17,9 @@ rb_ui2 <- function() {
     nav_panel(
       title = "One", 
       p("First page content."),
-      filters_ui()
+      filters_ui('filters'),
+      
+      gt::gt_output('salary_stats_text')
     ),
     nav_panel(title = "Two", p("Second page content.")),
     nav_spacer(),
@@ -25,4 +29,23 @@ rb_ui2 <- function() {
       nav_item(link_github)
     )
   )
+}
+
+#' updated server function
+#' @export
+rb_server_2 <- function(input, output, session) {
+  
+  .salaries <- filters_server('filters')
+  
+  # table salary stats
+  output$salary_stats_text <- gt::render_gt({
+    if (nrow(.salaries()) > 0) {
+      gt_salary_stats(.salaries())
+    } else {
+      tibble('No matching salary data' = '') |> gt::gt() |>
+        gt::tab_options(
+          table.border.top.style = "hidden",
+          table.border.bottom.style = "hidden")
+    }
+  })
 }
--- R/filters.R
+++ R/filters.R
@@ -1,16 +1,16 @@
 #' Filters, dropdowns ui
 #' @export
-filters_ui <- function() {
+filters_ui <- function(id, ...) {
   tagList(
     shiny.fluent::Dropdown.shinyInput(
-      'title',
+      NS(id, 'title'),
       multiSelect = TRUE,
       value = 'Scientist',
       label = NULL,
       options = salaries |> make_grouped_options(title_category, title_general)
     ),
     shiny.fluent::Dropdown.shinyInput(
-      "location_country",
+      NS(id, "location_country"),
       placeHolder = "Select location",
       multiSelect = FALSE,
       value = 'United States Of America',
@@ -21,7 +21,7 @@ filters_ui <- function() {
         arrange(key)
     ),
     shiny.fluent::Dropdown.shinyInput(
-      inputId = "location_granular",
+      inputId = NS(id, "location_granular"),
       placeHolder = "Select sub-location",
       multiSelect = TRUE
     ),
@@ -29,21 +29,89 @@ filters_ui <- function() {
     shiny.fluent::Stack(
       horizontal = TRUE,
       shiny.fluent::DefaultButton.shinyInput(
-        "select_all", text = "Select all"
+        NS(id, "select_all"), text = "Select all"
       ),
       shiny.fluent::DefaultButton.shinyInput(
-        "deselect_all", text = "Deselect all"
+        NS(id, "deselect_all"), text = "Deselect all"
       )
     ),
     shiny.fluent::Stack(
       horizontal = TRUE,
       tokens = list(childrenGap = 10),
       shiny.fluent::ChoiceGroup.shinyInput(
-        'date',
+        NS(id, 'date'),
         options = c('All' , '2024', '2023', '2022') %>%
           tibble(key = ., text = .),
         styles = list(flexContainer = list(display = "flex", gap = 10)),
         value = 'All')
     )
   )
+}
+
+#' filter server
+#' @export
+filters_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    
+    # location input reactives
+    .location_granular <- eventReactive(input$location_country, {
+      salaries |>
+        filter(location_country %in% input$location_country) |>
+        mutate(key = location_granular, text = location_granular ) |>
+        select(key, text) |>
+        distinct()  |>
+        arrange(key) |>
+        mutate(across(everything(), as.character))
+    })
+    
+    
+    observe({
+      shiny.fluent::updateDropdown.shinyInput(
+        session = session,
+        inputId = 'location_granular',
+        multiSelect = TRUE,
+        value = .location_granular() |>  pull(key),
+        options =  .location_granular()
+      )
+    })
+    observeEvent(input$deselect_all, {
+      shiny.fluent::updateDropdown.shinyInput(
+        session = session,
+        inputId = 'location_granular',
+        multiSelect = TRUE,
+        value = NULL,
+        options =  .location_granular()
+      )
+    })
+    observeEvent(input$select_all, {
+      shiny.fluent::updateDropdown.shinyInput(
+        session = session,
+        inputId = 'location_granular',
+        multiSelect = TRUE,
+        value = .location_granular() |>  pull(key),
+        options =  .location_granular()
+      )
+    })
+    
+    # return filtered salary data
+    reactive({
+      req(input$title)
+      .date <- switch(
+        input$date,
+        'All' = c('2024', '2023', '2022'),
+        '2024' = '2024',
+        '2023' = '2023',
+        '2022' = '2022'
+      )
+      
+      salaries |>
+        filter(
+          lubridate::year(date) %in% .date,
+          title_general %in% input$title,
+          location_country %in% input$location_country,
+          location_granular %in% input$location_granular
+        )
+    })
+    
+  })
 }
