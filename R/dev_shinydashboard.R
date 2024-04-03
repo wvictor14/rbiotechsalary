8cfc8055904f3f73d9f84acf892da59eae5c7aee
Victor Yuan <victor.2wy@gmail.com>
2024-04-02 23:15:04 -0700

    finish porting over remaining plots and tables

--- R/dev_shinydashboard.R
+++ R/dev_shinydashboard.R
@@ -18,7 +18,13 @@ rb_ui2 <- function() {
       title = "One", 
       p("First page content."),
       filters_ui('filters'),
-      table_salary_stats_ui('table_salary_stats')
+      table_salary_stats_ui('table_salary_stats'),
+      
+      plotly::plotlyOutput("plot"),
+      
+      gt::gt_output('table_career_progression'),
+      plotly::plotlyOutput('plot_experience'),
+      table_raw_ui("table_raw")
     ),
     nav_panel(title = "Two", p("Second page content.")),
     nav_spacer(),
@@ -36,5 +42,32 @@ rb_server_2 <- function(input, output, session) {
   
   .salaries <- filters_server('filters')
   table_salary_stats_server('table_salary_stats', .salaries)
+  
+  
+  output$plot <- plotly::renderPlotly({
+    plot_salary(.salaries(), title = 'Total Compensation (Base + Bonus)')
+  })
+  
+  
+  output$table_career_progression <- gt::render_gt({
+    gt_career_progression(.salaries())
+  })
+  output$plot_experience <- plotly::renderPlotly({
+    
+    # suppress warnings for app session
+    storeWarn<- getOption("warn")
+    options(warn = -1)
+    
+    if (nrow(.salaries()) > 0) {
+      plot_experience(.salaries())
+    } else {
+      p <- ggplot() + theme_minimal(); plotly::ggplotly(p, height = 250)
+    }
+  })
+  
+  # render the table + other components
+  table_raw_server('table_raw', .salaries)
+  
+  
   
 }
