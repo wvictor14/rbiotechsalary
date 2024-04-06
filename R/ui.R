871b27b42ea8f701b286f652f1a630e197afbe8b
Victor Yuan <victor.2wy@gmail.com>
2024-04-06 10:42:03 -0700

    move table to own panel

--- R/ui.R
+++ R/ui.R
@@ -9,11 +9,29 @@
 #'   launch_app(ui = rb_ui, server = rb_server)
 #' }
 rb_ui <- function() {
-  link_github <- tags$a(
-    shiny::icon("github"), "source",
+  
+  link_add_data <- tags$a(
+    bsicons::bs_icon("plus-lg"), " Add Your Salary",
+      href = "https://docs.google.com/forms/d/e/1FAIpQLSeXpPvkjdf8EPRU_cAdyMAUFVjgx67nyVgDxV7TNHeFkR2k9A/viewform",
+      target = "_blank"
+    )
+     link_github <- tags$a(
+    shiny::icon("github"), " Source",
     href = "https://github.com/wvictor14/rbiotechsalary",
     target = "_blank"
   )
+  link_reddit <- tags$a(
+    bsicons::bs_icon("reddit"), " Reddit",
+    href = "https://www.reddit.com/r/biotech/comments/125cy06/rbiotech_salary_and_company_survey/",
+    target = "_blank"
+  )
+  link_google <- tags$a(
+    bsicons::bs_icon("file-earmark-spreadsheet"), " Raw Data",
+    href = "https://docs.google.com/spreadsheets/d/1G0FmJhkOME_sv66hWmhnZS5qR2KMTY7nzkxksv46bfk/edit#gid=491268892",
+    target = "_blank"
+  )
+  
+  
   .colors <- list(
     'primary' = '#41AB5DFF'
   )
@@ -24,7 +42,7 @@ rb_ui <- function() {
       bg = '#EEE8D5',
       primary = .colors$primary
     ),
-    title = "r/biotech salary",
+    title = "r/biotech salaries",
     fillable = FALSE,
     
     sidebar = sidebar(
@@ -39,16 +57,14 @@ rb_ui <- function() {
       h1(textOutput('content_title')),
       layout_columns(value_boxes_stats_ui('value_boxes')),
       plotly::plotlyOutput("plot_salary_histogram"),
-      card(
-        full_screen = TRUE,
-        height = '80vh',
-        card_body(
-          table_raw_ui("table_raw")
-        )
-      )
     ),
     
-    ### panel 2 ----
+    nav_panel(
+      title = 'Raw data',
+      h1(textOutput('content_title')),
+      table_raw_ui("table_raw")
+    ),
+    ### panel career progression ----
     nav_panel(
       title = "Career progression", 
       card(
@@ -63,7 +79,11 @@ rb_ui <- function() {
     nav_menu(
       title = "Links",
       align = "right",
+      nav_item(link_add_data),
+      nav_item(link_google),
+      nav_item(link_reddit),
       nav_item(link_github)
+      
     ),
     nav_item(
       input_dark_mode(id = "dark_mode", mode = "dark")
