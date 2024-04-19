519c1aadcdf66c864f93da46f99124eb34e01da2
Victor Yuan <victor.2wy@gmail.com>
2024-04-18 22:03:25 -0700

    remove gt in launch_app

--- R/launch_app.R
+++ R/launch_app.R
@@ -17,18 +17,12 @@ launch_app <- function(options = list(), ui = rb_ui, server = rb_server, ...) {
     skimr::skim() |> 
     as_tibble() |> 
     rename_with(~stringr::str_remove_all(.x, 'character\\.')) |> 
-    select(-skim_type, -whitespace) |> 
-    gt::gt() |> 
-    gt_dark_mode() |> 
-    gt::cols_label(
-      skim_variable = 'Survey Response Variable'
-    )
+    select(-skim_type, -whitespace) 
   
   message('data summarized with skimr')
   
   data(salaries)
   message('processed data loaded')
-  
   
   version <- paste0('v', as.character(utils::packageVersion('rbiotechsalary')))
   #shiny::addResourcePath("www", "www")
