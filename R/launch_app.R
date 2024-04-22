ebe17049b61d1fb7194ab42c935522aeaf03a75f
Victor Yuan <victor.2wy@gmail.com>
2024-04-22 11:02:58 -0700

    try one last time with old framework

--- R/launch_app.R
+++ R/launch_app.R
@@ -12,7 +12,7 @@
 #'  launch_app(server = function(input, output) {})
 #'  
 #' }
-launch_app <- function(options = list(), ui = rb_ui, server = NULL, ...) {
+launch_app <- function(...) {
   
   # load data for app
   skim_raw_data <- load_raw_data() |> 
@@ -23,7 +23,11 @@ launch_app <- function(options = list(), ui = rb_ui, server = NULL, ...) {
   
   message('data summarized with skimr')
   
-  data(salaries)
+  salaries <- readr::read_csv(
+    here::here('data', 'salary_results_cleaned.csv'),
+    show_col_types = FALSE
+  ) |> 
+    rb_relevel()
   message('processed data loaded')
   
   version <- paste0('v', as.character(utils::packageVersion('rbiotechsalary')))
@@ -31,8 +35,14 @@ launch_app <- function(options = list(), ui = rb_ui, server = NULL, ...) {
   
   # source server.R in this environment to gain access to above variables
   source(here::here('R', 'server.R'), local = TRUE)
+  source(here::here('R', 'ui.R'), local = TRUE)
   
   # run app
   message('Running rbiotechsalary version ', version)
-  shiny::shinyApp(ui, rb_server, options = list(launch.browser = TRUE), ...)
+  shiny::shinyApp(
+    rb_ui(salaries), 
+    rb_server, 
+    options = list(launch.browser = TRUE), 
+    ...
+  )
 }
